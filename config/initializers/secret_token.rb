616392e47857b51afccfff12e653e073d45b3a77
Constantin Hofstetter <Consti@Consti.de>
2012-06-29 13:33:40 +0200

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Betterberg::Application.config.secret_token = 'e931be1b302a97ff29c6a8f056024279a53c45c77e884690302744b26422587709b0bbbc8d800df13655725f484461b1b13c92a3f993b102721edfe053f5bded'
